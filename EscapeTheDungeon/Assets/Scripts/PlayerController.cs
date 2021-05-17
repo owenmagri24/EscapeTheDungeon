3bd3cc1205a7227a318a6e5c3040c285d6a3042c
owenmagri24 <74650731+owenmagri24@users.noreply.github.com>
2021-05-17 12:17:19 +0200

    Started Project and Lights
    - Started URP Project
    - Created Player movement script
    - Created Global Lights
    - Imported Sample assets to work with

---
+++ EscapeTheDungeon/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed = 5f;
+
+    [SerializeField]
+    Rigidbody2D rb;
+
+    Vector2 movement;
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate() {
+        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+    }
+}
