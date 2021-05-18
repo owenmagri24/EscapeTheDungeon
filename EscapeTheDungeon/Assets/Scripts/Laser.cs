00f12a4ce6ad8b7b73a3fb50bd679a95d106d1ed
Pietru0 <74650785+Pietru0@users.noreply.github.com>
2021-05-18 13:42:10 +0200

    Rotation left/right implemented
    Working on rotating direction where laser shoots
    Also working on destroying layer upon collision with platform

---
+++ EscapeTheDungeon/Assets/Scripts/Laser.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    public float speed = 8.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+        //set laser boundaries after
+    }
+
+    void OnCollisionEnter2D(Collision collision)
+    {
+        if (collision.gameObject.tag == "Platform")
+        {
+            if (transform.parent != null)
+            {
+                Destroy(transform.parent.gameObject);
+            }
+            Destroy(gameObject);
+        }
+    }
+}
--- EscapeTheDungeon/Assets/Scripts/PlayerController.cs
+++ EscapeTheDungeon/Assets/Scripts/PlayerController.cs
@@ -10,15 +10,70 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     Rigidbody2D rb;
 
+    [SerializeField]
+    private GameObject _laser;
+
+    public Transform firePoint;
+
     Vector2 movement;
 
+    public bool isFacingLeft = true;
+
+    public Laser laser;
+
+    void Start()
+    {
+        laser = GameObject.Find("Laser").GetComponent<Laser>();
+        if (laser = null)
+        {
+            Debug.LogError("Laser not found ??");
+        }
+    }
+    
     void Update()
     {
+        //controls
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (isFacingLeft)
+            {
+                flip();
+                laser.transform.Translate(Vector3.right * laser.speed * Time.deltaTime);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (!isFacingLeft)
+            {
+                flip();
+            }
+        }
     }
 
     void FixedUpdate() {
         rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+    }
+
+    void Shoot()
+    {
+        Instantiate(_laser, firePoint.position, firePoint.rotation);
+    }
+
+    //flip chara left
+    void flip()
+    {
+        isFacingLeft = !isFacingLeft;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
