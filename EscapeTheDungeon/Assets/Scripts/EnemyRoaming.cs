7dc6d11312f874cc4b4b758da617276d3bc67174
Pietru0 <74650785+Pietru0@users.noreply.github.com>
2021-05-22 15:09:08 +0200

    Enemy Patrol partially done
    - Enemy is currently walking to 1 target out of the set 4
    
    - Will need to check in further detail how arrays work
    
    -AIPath and AIDestinationSetter are linked to enemy already

---
+++ EscapeTheDungeon/Assets/Scripts/EnemyRoaming.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyRoaming : MonoBehaviour
+{
+    GameObject[] targets;
+    
+    [SerializeField]
+    int currentTarget;
+    [SerializeField]
+    float minDistance = 0.1f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targets = GameObject.FindGameObjectsWithTag("Target");
+        gameObject.GetComponent<AIDestinationSetter>().target = targets[currentTarget].transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(this.transform.position,targets[currentTarget].transform.position);
+
+        if (distance <=minDistance)
+        {
+            currentTarget++;
+
+            if (currentTarget == targets.Length)
+            {
+                currentTarget = 0;
+            }
+
+            gameObject.GetComponent<AIDestinationSetter>().target = targets[currentTarget].transform;
+        }
+    }
+}
+
