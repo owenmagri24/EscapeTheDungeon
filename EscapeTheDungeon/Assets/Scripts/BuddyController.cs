dcde417e68c5caacb720f174fab2211fd1a7feb2
owenmagri24 <74650731+owenmagri24@users.noreply.github.com>
2021-05-26 15:03:53 +0200

    Started Medkit System
    -- Implemented array for medkit prefabs
    -- Buddy goes for medkit when in rage
    -- Returns to player on collision with medkit
    -- still needs tuning as currently he goes to the medkit according to the array order

---
+++ EscapeTheDungeon/Assets/Scripts/BuddyController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class BuddyController : MonoBehaviour
+{
+    [SerializeField]
+    private float buddyMedkitDistance;
+
+    GameObject[] medkit;
+
+    [SerializeField]
+    private int currentTarget;
+    public Transform player;
+
+    public AIDestinationSetter aiDestinationSetter;
+
+    public AIPath aIPath;
+
+    private bool medkitAvailable = true;
+
+
+    void Start()
+    {
+        medkit = GameObject.FindGameObjectsWithTag("MedKit");
+    }
+
+    
+    void Update()
+    {
+        if(medkitAvailable == true){
+            float distance = Vector2.Distance(transform.position, medkit[currentTarget].transform.position);
+
+            if(distance <= buddyMedkitDistance)
+            {
+                aiDestinationSetter.GetComponent<AIDestinationSetter>().target = medkit[currentTarget].transform;
+                aIPath.GetComponent<AIPath>().endReachedDistance = 1f;
+            }
+
+        }
+        
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "MedKit")
+        {
+            Destroy(other.gameObject);
+            currentTarget++;
+
+            if(currentTarget == medkit.Length)
+            {
+                medkitAvailable = false;
+            }
+
+            aiDestinationSetter.GetComponent<AIDestinationSetter>().target = player.transform;
+            aIPath.GetComponent<AIPath>().endReachedDistance = 3f;
+        }
+    }
+}
